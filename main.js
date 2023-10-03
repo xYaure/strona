0e4dc2b7b20f77fe2ff2ac4b390a39b9888359c4
xYaure <sebastianklak6@gmail.com>
2023-10-03 18:37:59 +0200

    Some changes

--- main.js
+++ main.js
@@ -25,8 +25,41 @@ setInterval(() => {
 
 const loading_screen = document.querySelector('#loading_screen');
 window.addEventListener('load',()=>{
-        loading_screen.style.opacity="0"
+        loading_screen.style.opacity="0";
         setTimeout(() => {
-            loading_screen.style.display="none"
+            loading_screen.style.display="none";
         }, 500);
+})
+
+const exp = document.querySelector('#exp');
+
+
+function fly_in(x, y) {
+    x.style.transform = `translateX(${y})`;
+    x.style.opacity = 1;
+}
+
+
+
+const bars = document.querySelectorAll("#exp ul li span");
+const li = document.querySelectorAll("#exp ul li");
+
+for (let i = 0; i < li.length; i++) {
+    let h = li[i].getAttribute('value');
+    bars[i].style.height = `${h}em`
+    
+}
+
+window.addEventListener("scroll",()=>{
+    let scroll = Math.floor(window.scrollY);
+    console.log(scroll)
+    if(scroll > 250)
+    {
+        fly_in(exp, 0);
+        setTimeout(() => {
+            bars.forEach(e => {
+                e.style.transform="translateY(0)";
+            });
+        }, 1500);
+    }
 })
